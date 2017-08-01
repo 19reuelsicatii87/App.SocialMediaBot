f10b239738853045c4793f61ff54fc69f9ebe570
unknown <reuel@prodtestPC.tlos.local>
2017-08-01 11:51:22 +0800

    Updated CompassSmokeFirstTest.java to run TS01 & TS04 only

--- src/test/java/cucumber/Runner/CompassSmokeFirstTest.java
+++ src/test/java/cucumber/Runner/CompassSmokeFirstTest.java
@@ -27,7 +27,7 @@ import com.cucumber.listener.*;
 		features = {"."},
 		glue = {"cucumber.Framework","webApp.Compass", "webApp.Seoreseller", "webApp.PayPerContent"},
         plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/CucumberReport.html","rerun:target/rerun.txt"},
-        tags = {"@COMSMOKETEST_TS01, @COMSMOKETEST_TS02, @COMSMOKETEST_TS06 "}
+        tags = {"@COMSMOKETEST_TS01, @COMSMOKETEST_TS04"}
    
 
 )
