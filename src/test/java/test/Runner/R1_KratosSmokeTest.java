636f59b362800ca7c52eb3dea7e7e95f7ce1de53
Reuel Sicat <reuel@truelogic.com.ph>
2017-08-31 13:40:34 +0800

    Rename Test Runner to correct the sequence of Test Execution

---
+++ src/test/java/test/Runner/R1_KratosSmokeTest.java
@@ -0,0 +1,50 @@
+package test.Runner;
+
+import java.io.IOException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.Utilities.Mail;
+
+import cucumber.Framework.Helper;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		format = { "pretty", "html:target/cucumber","json:target/JSON/Output.json" },
+		features = {"."},
+		glue = {"webApi.Kratos", "restassured.Framework"},
+        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/KratosReport.html","rerun:target/rerun_apikratos.txt"},
+        tags = {"@KRATOSSMOKETEST"}
+
+   
+
+)
+
+
+public class R1_KratosSmokeTest extends Helper {
+	
+	@BeforeClass
+	public static void BeforeClass() throws Exception
+	{
+		log.info("Execution is started from First Runner Test - BeforeClass Annotation");
+	}	
+		
+ 	
+	@AfterClass
+	public static void AfterClass() throws IOException, Throwable
+	{	
+		Mail.SendReport("KratosReport.html", "KratosDEVURL", "[SMOKE TEST]: KRATOS - ");		
+		log.info("Execution is ended from First Runner - Test AfterClass Annotation");
+		
+	}
+}
+
+
